6a3b85306252ef245171471c562d9f6d35ecf1da
rubyyb <ruby_binn@naver.com>
2024-12-13 18:24:16 +0900

    ✅ 복사 기능 추가 및 이미지 조작 막기

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+// particlesJS.load("particles-js", "assets/particles.json", function () {
+//   console.log("callback - particles.js config loaded");
+// });
+
+function copyToClipboard(elementId) {
+  // 복사할 요소 선택
+  const textToCopy = document.getElementById(elementId).innerText;
+
+  // 대시 제거
+  const sanitizedText = textToCopy.replace(/-/g, "");
+
+  // 클립보드에 복사
+  navigator.clipboard
+    .writeText(sanitizedText)
+    .then(() => {
+      alert("계좌 번호가 복사되었습니다");
+    })
+    .catch((err) => {
+      alert("복사에 실패했습니다. 다시 시도해주세요.");
+      console.error("복사 실패:", err);
+    });
+}
--- public/js/fancybox.js
+++ public/js/fancybox.js
@@ -1,19 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    Fancybox.bind("[data-fancybox]", {
-        zoom: false,
-        dragToClose: false,
-        doubleClick: false,
-        wheel: false,
-        on: {
-            "click": function (fancybox, event) {
-            event.preventDefault();
-            },
-            "wheel": function (fancybox, event) {
-            event.preventDefault();
-            },
-            "dblclick": function (fancybox, event) {
-            event.preventDefault();
-            },
-        },
-    });
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    zoom: false,
+    dragToClose: false,
+    doubleClick: false,
+    wheel: false,
+    on: {
+      click: function (fancybox, event) {
+        event.preventDefault();
+      },
+      wheel: function (fancybox, event) {
+        event.preventDefault();
+      },
+      dblclick: function (fancybox, event) {
+        event.preventDefault();
+      },
+    },
+    Toolbar: {
+      display: {
+        left: [],
+        middle: ["prev", "infobar", "next"],
+        right: ["close"],
+      },
+    },
+    Image: {
+      click: false, // 클릭 시 확대 비활성화
+      wheel: false, // 마우스 휠 확대 비활성화
+    },
+  });
 });
